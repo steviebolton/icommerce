47e98cdf07af2c8b608022d99c779186247968bd
Steven John Bolton <steviebolton@gmail.com>
2018-12-31 15:56:53 +0000

    home added

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'cart',
     'checkout',
     'storages',
+    'home'
 ]
 
 MIDDLEWARE = [
--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
 # Create your models here.
+
+    
+
+class HomeCategorie(models.Model):
+    home_categorie_type = models.CharField(max_length=15, blank=False, null=False)
+    name= models.CharField(max_length=40, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+   
+    def __str__(self):
+        return self.name
+        
+        
+class homeImage(models.Model):
+    home_image = models.ImageField(upload_to="home_images" , null=True, blank=True)
+   
+
+    def __str__(self):
+        return self.home.name
